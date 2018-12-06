96b96e0b80fcf5591f581293aeeb5c3e3cd338b3
maxLundin <242440@niuifmo.ru>
2018-12-06 22:51:23 +0300

    1

--- filedigger.cpp
+++ filedigger.cpp
@@ -7,62 +7,62 @@
 #include <thread>
 
 
-FileDigger::FileDigger(QString dir)
-{
+FileDigger::FileDigger(QString dir) {
     working_dir = dir;
     is_scanning_now = false;
 }
 
-void FileDigger::check_files_eq(FileDigger * that,
+void FileDigger::check_files_eq(FileDigger *that,
                                 std::map<int64_t, std::vector<QFileInfo>> *array,
-                                bool * thread_end,
+                                bool *thread_end,
                                 std::vector<std::pair<QFileInfo, bool> > *do_out,
-                                std::mutex * mutex) {
+                                std::mutex *mutex) {
     std::vector<std::vector<size_t>> equal_files;
     uint64_t eq_files_groups = 0;
     bool used_groups = true;
     int files_checked = 0;
     char buffer1[buffer_mas_length];
     char buffer2[buffer_mas_length];
-    for (auto i = (*array).rbegin(); i != (*array).rend(); ++i){
-        auto& elem = (*i);
+    for (auto i = (*array).rbegin(); i != (*array).rend(); ++i) {
+        auto &elem = (*i);
 #ifdef DEBUG
         std::cout << "doing new group" << std::endl;
 #endif
-        if (!that->is_scanning_now){
+        if (!that->is_scanning_now) {
             return;
         }
         eq_files_groups = (equal_files).size();
 
         if (elem.second.size() > 1) {
-            if (elem.first != 0){
+            if (elem.first != 0) {
                 std::vector<std::unique_ptr<std::ifstream>> filesin;
                 filesin.reserve(elem.second.size());
 #ifdef DEBUG
                 std::vector<std::string> mas_strings;
 #endif
 
-                for (std::vector<std::unique_ptr<std::ifstream>>::size_type i = 0; i < elem.second.size(); i++) {
+                for (size_t i = 0; i < elem.second.size(); i++) {
 #ifdef DEBUG
                     mas_strings.push_back(elem.second[i].absoluteFilePath().toStdString());
                     std::cout << "open file -> " << mas_strings.back() << std::endl;
 #endif
                     std::unique_ptr<std::ifstream> temp;
                     temp.reset(new std::ifstream(elem.second[i].absoluteFilePath().toStdString(), std::ios::binary));
-                    if(temp->is_open()){
+                    if (temp->is_open()) {
                         (filesin).push_back(std::move(temp));
-                    }else{
+                    } else {
                         std::cout << "Error opening file -> " <<
                                      elem.second[i].absoluteFilePath().toStdString() << std::endl;
-                        for (int _ = 0 ; _ < 3; _ ++){
+                        for (int _ = 0; _ < 3; _++) {
                             std::cout << "Trying again -> " <<
                                          elem.second[i].absoluteFilePath().toStdString() << std::endl;
-                            temp.reset(new std::ifstream(elem.second[i].absoluteFilePath().toStdString(), std::ios::binary));
-                            if(temp->is_open()){
+                            temp.reset(new std::ifstream(elem.second[i].absoluteFilePath().toStdString(),
+                                                         std::ios::binary));
+                            if (temp->is_open()) {
                                 (filesin).push_back(std::move(temp));
                                 std::cout << "It opened!!!" << std::endl;
                                 break;
-                            }else{
+                            } else {
                                 std::cout << "Error" << std::endl;
                             }
                         }
@@ -72,15 +72,15 @@ void FileDigger::check_files_eq(FileDigger * that,
                 std::vector<bool> used_files(elem.second.size());
 
                 bool flag_eq = false;
-                for (std::vector<std::unique_ptr<std::ifstream>>::size_type i = 0; i < filesin.size(); i++) {
+                for (size_t i = 0; i < filesin.size(); i++) {
                     if (used_files[i]) {
                         continue;
                     }
                     used_files[i] = true;
                     bool added = false;
                     flag_eq = true;
-                    for (std::vector<std::unique_ptr<std::ifstream>>::size_type j = i + 1; j < (filesin).size(); j++) {
-                        if (used_files[j]){
+                    for (size_t j = i + 1; j < (filesin).size(); j++) {
+                        if (used_files[j]) {
                             continue;
                         }
                         (*filesin[i]).clear();
@@ -91,9 +91,10 @@ void FileDigger::check_files_eq(FileDigger * that,
                             (*filesin[i]).read(buffer1, buffer_mas_length * sizeof(char));
                             (*filesin[j]).read(buffer2, buffer_mas_length * sizeof(char));
                             std::streamsize buff_length = (*filesin[i]).gcount();
-                            for (std::streamsize k = 0; flag_eq && k < buff_length; k++) {
-                                flag_eq = (buffer1[k] == buffer2[k]);
-                            }
+                            flag_eq = (memcmp(buffer1,buffer2,buff_length) == 0);
+                            //                            for (std::streamsize k = 0; flag_eq && k < buff_length; k++) {
+                            //                                flag_eq = (buffer1[k] == buffer2[k]);
+                            //                            }
                         }
                         if (flag_eq) {
                             if (!added) {
@@ -112,16 +113,16 @@ void FileDigger::check_files_eq(FileDigger * that,
                     std::cout << "close file -> " << mas_strings[i] << std::endl;
 #endif
                 }
-                for (uint64_t h = eq_files_groups ; h < (equal_files).size(); h++){
-                    for (std::vector<std::vector<QFileInfo>::size_type>::size_type i = 0; i < (equal_files)[h].size(); i++) {
+                for (uint64_t h = eq_files_groups; h < (equal_files).size(); h++) {
+                    for (size_t i = 0; i < (equal_files)[h].size(); i++) {
                         mutex->lock();
                         (*do_out).push_back(std::make_pair(std::move(elem.second[(equal_files)[h][i]]), used_groups));
                         mutex->unlock();
                     }
                     used_groups = !used_groups;
                 }
-            }else{
-                for (uint64_t h = 0 ; h < elem.second.size(); h++){
+            } else {
+                for (uint64_t h = 0; h < elem.second.size(); h++) {
                     mutex->lock();
                     (*do_out).push_back(std::make_pair(std::move(elem.second[h]), used_groups));
                     mutex->unlock();
@@ -131,7 +132,7 @@ void FileDigger::check_files_eq(FileDigger * that,
         }
         (files_checked) += elem.second.size();
         emit that->status(files_checked);
-        if (!that->is_scanning_now){
+        if (!that->is_scanning_now) {
             return;
         }
     }
@@ -139,7 +140,7 @@ void FileDigger::check_files_eq(FileDigger * that,
 }
 
 
-void FileDigger::do_file_search(){
+void FileDigger::do_file_search() {
     is_scanning_now = true;
     QDir dira = QDir(working_dir);
     std::map<int64_t, std::vector<QFileInfo>> same_size;
@@ -176,26 +177,25 @@ void FileDigger::do_file_search(){
     is_scanning_now = false;
 }
 
-void FileDigger::stop_scanning()
-{
+void FileDigger::stop_scanning() {
     is_scanning_now = false;
 }
 
 
-
-void FileDigger::make_groups(FileDigger * that, QDir &dir, std::map<int64_t, std::vector<QFileInfo>> *array, int *files_in_directory) {
+void FileDigger::make_groups(FileDigger *that, QDir &dir, std::map<int64_t, std::vector<QFileInfo>> *array,
+                             int *files_in_directory) {
     QFileInfoList list = dir.entryInfoList();
     for (QFileInfo &file_info : list) {
 #ifdef DEBUG
         std::cout << "Doing groups in -> " << file_info.absoluteFilePath().toStdString() << std::endl;
 #endif
-        if (!that->is_scanning_now){
+        if (!that->is_scanning_now) {
             return;
         }
         if (file_info.fileName() == "." || file_info.fileName() == "..") {
             continue;
         }
-        if (file_info.isSymLink()){
+        if (file_info.isSymLink()) {
             continue;
         }
         if (file_info.isDir()) {
@@ -213,37 +213,37 @@ void FileDigger::make_groups(FileDigger * that, QDir &dir, std::map<int64_t, std
                 (*array).insert(std::make_pair(file_info.size(), temp));
             }
         }
-        if (!that->is_scanning_now){
+        if (!that->is_scanning_now) {
             return;
         }
     }
 }
 
 
-void FileDigger::add_to_ui(FileDigger * that, bool *thread_end,
+void FileDigger::add_to_ui(FileDigger *that, bool *thread_end,
                            std::vector<std::pair<QFileInfo, bool>> *out_queue,
-                           std::mutex *mutex){
+                           std::mutex *mutex) {
     int files_added = 0;
     bool current_bool = true;
     bool finished = true;
     std::unique_ptr<QTreeWidgetItem> itemFather;
     std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    while (!(*thread_end)){
+    while (!(*thread_end)) {
         // add group
 
 
         mutex->lock();
-        if ((*out_queue).size()){
+        if ((*out_queue).size()) {
             std::vector<std::pair<QFileInfo, bool>> my_queue;
             my_queue.reserve(((*out_queue).size()));
             (*out_queue).swap(my_queue);
             mutex->unlock();
             size_t pos = 0;
-            if (finished){
+            if (finished) {
                 itemFather.reset(new QTreeWidgetItem());
             }
-            while (pos != my_queue.size()){
-                while (pos != my_queue.size() && my_queue[pos].second == current_bool){
+            while (pos != my_queue.size()) {
+                while (pos != my_queue.size() && my_queue[pos].second == current_bool) {
                     QTreeWidgetItem *item1 = new QTreeWidgetItem();
                     ++files_added;
                     item1->setText(0, my_queue[pos].first.path());
@@ -252,25 +252,25 @@ void FileDigger::add_to_ui(FileDigger * that, bool *thread_end,
                     itemFather->addChild(item1);
                     ++pos;
                 }
-                if (pos != my_queue.size()){
+                if (pos != my_queue.size()) {
                     finished = true;
-                    if (itemFather->childCount() == 0){
-                           std::cout << "Something is wrong" << std::endl;
-                    }else{
-                        itemFather->setText(0,"Number of copies : " + QString::number(itemFather->childCount()));
-                        itemFather->setText(2,"Size : " + itemFather->child(0)->text(2) + "bytes.");
+                    if (itemFather->childCount() == 0) {
+                        std::cout << "Something is wrong" << std::endl;
+                    } else {
+                        itemFather->setText(0, "Number of copies : " + QString::number(itemFather->childCount()));
+                        itemFather->setText(2, "Size : " + itemFather->child(0)->text(2) + "bytes.");
                         emit that->ready_to_add(itemFather.get());
                         itemFather.release();
                         itemFather.reset(new QTreeWidgetItem());
                         current_bool = !current_bool;
                     }
-                }else{
+                } else {
                     finished = false;
                 }
             }
 
 
-        }else{
+        } else {
             mutex->unlock();
             std::this_thread::sleep_for(std::chrono::milliseconds(50));
         }
@@ -279,11 +279,11 @@ void FileDigger::add_to_ui(FileDigger * that, bool *thread_end,
 
     size_t pos = 0;
 
-    if (finished){
+    if (finished) {
         itemFather.reset(new QTreeWidgetItem());
     }
-    while (pos!=(*out_queue).size()){
-        while (pos != (*out_queue).size() && (*out_queue)[pos].second == current_bool){
+    while (pos != (*out_queue).size()) {
+        while (pos != (*out_queue).size() && (*out_queue)[pos].second == current_bool) {
             QTreeWidgetItem *item1 = new QTreeWidgetItem();
             ++files_added;
             item1->setText(0, (*out_queue)[pos].first.path());
@@ -293,8 +293,8 @@ void FileDigger::add_to_ui(FileDigger * that, bool *thread_end,
             ++pos;
         }
         finished = true;
-        itemFather->setText(0,"Number of copies : " + QString::number(itemFather->childCount()));
-        itemFather->setText(2,"Size : " + itemFather->child(0)->text(2) + "bytes.");
+        itemFather->setText(0, "Number of copies : " + QString::number(itemFather->childCount()));
+        itemFather->setText(2, "Size : " + itemFather->child(0)->text(2) + "bytes.");
         emit that->ready_to_add(itemFather.get());
         itemFather.release();
         itemFather.reset(new QTreeWidgetItem());
